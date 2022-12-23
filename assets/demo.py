daaabd0b3ada819dcb740783038cfc9404d5d282
JayShankar99 <72004719+JayShankar99@users.noreply.github.com>
2022-12-23 09:50:49 +0530

    initialize repo
    we have created two folders and two files

---
+++ assets/demo.py
@@ -0,0 +1,2 @@
+def fun(num1, num2):
+    return num1+num2
---
+++ main.py
@@ -0,0 +1,7 @@
+from assets import demo
+
+def sum_of_two_num(a,b):
+    print(demo.fun(a,b))
+
+if __name__ =="__main__":
+    sum_of_two_num(12,10)
